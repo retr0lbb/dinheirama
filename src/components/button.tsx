06ee0ff379f44ce1aebbc7b4c3d5b36b6c6b87bb
Henrique Barbosa Sampaio <retr0lbb@gmail.com>
2024-08-18 02:11:39 -0300

    feat: add tailwind variants

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,10 +1,16 @@
 import { ComponentProps } from "react"
+import { tv } from "tailwind-variants"
 
 interface ButtonProps extends ComponentProps<"button"> {}
 
-export function Button({children}: ButtonProps){
+
+const button = tv({
+    base: "bg-neutral-700 text-neutral-200 p-2.5 rounded-xl hover:bg-neutral-800 text-neutral-100 shadow transition-all scale-105"
+})
+
+export function Button({children, className}: ButtonProps){
     return(
-        <button className="bg-neutral-700 text-neutral-200 p-2.5 rounded-xl">
+        <button className={button({className: className})}>
             {children}  
         </button>
     )
