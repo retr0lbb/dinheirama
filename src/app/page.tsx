4c8ba5dd63684ffb0a2326250e65308227bc3391
retr0lbb <retr0lbb@gmail.com>
2024-08-19 22:28:45 +0000

    feat: remove japan letter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,10 @@ export default function Home() {
   return (
     <main className="h-screen flex items-center flex-col justify-center bg-neutral-950">
       <FreeFallingBanknotes isPlaying={true} />
-      <div className="flex flex-col items-center justify-center space-y-8 z-10">
+      <div className="flex flex-col items-center justify-center space-y-2 z-10">
         <h1 className="text-8xl font-bold text-neutral-300 text-center">
-          <strong className="text-green-600 font-bold text-9xl">金</strong><br></br>Dinheirama
+          Dinheirama
         </h1>
-
         <p className="text-neutral-500 text-xl max-w-96 text-center">Chega de perder dinheiro com jogos de aposta baratos, mude de vida agora mesmo.</p>
       </div>
 
