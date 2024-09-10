caf4e272fb087df1cc8bb27ec90a7c58f6d79cfe
retr0lbb <retr0lbb@gmail.com>
2024-09-10 20:54:25 -0300

    all

--- src/components/stacked-card.tsx
+++ src/components/stacked-card.tsx
@@ -4,7 +4,7 @@
 export function StackedBankCards(){
     return(
         <div className="bg-gradient-to-br from-strong_violet to-dang_blue w-full h-full ">
-
+            
         </div>
     )
 }
