e7975d758fd09b8d16f1cbbbcc5e6e6bc962c2c7
retr0lbb <retr0lbb@gmail.com>
2024-09-22 14:37:50 -0300

    feat: add react hook form

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,14 +1,36 @@
+'use client'
 import { Button } from '@/components/button'
 import { Input } from '@/components/input'
-import { LinkComponent } from '@/components/link'
 import { FaApple, FaGoogle } from 'react-icons/fa'
 import Link from 'next/link'
 import { FaXTwitter } from 'react-icons/fa6'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+const registerLogin = z.object({
+  name: z.string().min(3),
+  email: z.string().email(),
+  phone: z.coerce.number().positive().min(10),
+  password: z.string().min(6).max(64),
+  confirmPassword: z.string().min(6).max(64),
+})
+
+interface RegisterLoginForm extends z.infer<typeof registerLogin> {}
 
 export default function Register() {
+  const { register, formState, handleSubmit, reset } =
+    useForm<RegisterLoginForm>({
+      resolver: zodResolver(registerLogin),
+    })
+
+  async function handleRegisterSubmit(data: RegisterLoginForm) {
+    console.log(data)
+    reset()
+  }
+
   return (
     <main className="h-screen bg-black flex items-center justify-center antialiased">
-      {/* main div */}
       <div className="space-y-8">
         <header className="flex items-center justify-center flex-col gap-2">
           <h1 className="text-5xl text-snow-400 font-bold">
@@ -19,51 +41,54 @@ export default function Register() {
           </p>
         </header>
 
-        <div className="space-y-5">
-          <form className="px-12 space-y-5" action="">
-            <div className="space-y-4">
+        <div className="flex flex-col gap-2">
+          <form
+            onSubmit={handleSubmit(handleRegisterSubmit)}
+            className="px-12 flex flex-col gap-4"
+          >
+            <div className="flex flex-col gap-2">
               <Input Title="Nome Completo">
                 <Input.Field
                   type="text"
-                  name="name"
                   id="name"
                   placeholder="Fulano da silva"
+                  {...register('name')}
                 />
               </Input>
 
               <Input Title="Numero de contato">
                 <Input.Field
                   type="number"
-                  name="tel"
                   id="tel"
                   placeholder="(01) 12345-6789"
+                  {...register('phone')}
                 />
               </Input>
 
               <Input Title="Email">
                 <Input.Field
                   type="email"
-                  name="email"
                   id="email"
                   placeholder="example@mail.com"
+                  {...register('email')}
                 />
               </Input>
 
               <Input Title="Senha">
                 <Input.Field
                   type="password"
-                  name="password"
                   id="password"
                   placeholder="Sua senha"
+                  {...register('password')}
                 />
               </Input>
 
               <Input Title="Confirmar senha">
                 <Input.Field
                   type="password"
-                  name="cpass"
                   id="cpass"
                   placeholder="Confirme sua senha"
+                  {...register('confirmPassword')}
                 />
               </Input>
             </div>
@@ -82,8 +107,6 @@ export default function Register() {
                 </Link>
               </span>
             </div>
-
-            {/* divider with button */}
           </form>
           <div className="w-full flex items-center justify-center text-snow-600 text-xs px-14">
             <div className="bg-snow-600 w-full h-px rounded" />
@@ -97,8 +120,7 @@ export default function Register() {
               sizes="sm"
               className="flex flex-row h-10 py-0 flex-grow-0 flex-shrink-0 w-auto gap-2 text-snow-600"
             >
-              <FaGoogle className="text-snow-400"></FaGoogle> Cadastre-se com o
-              Google.
+              <FaGoogle className="text-snow-400" /> Cadastre-se com o Google.
             </Button>
 
             <Button
@@ -106,8 +128,7 @@ export default function Register() {
               sizes="sm"
               className="flex flex-row h-10 py-0 flex-grow-0 flex-shrink-0 w-auto gap-2 text-snow-600"
             >
-              <FaXTwitter className="text-snow-400"></FaXTwitter> Cadastre-se
-              com Twitter.
+              <FaXTwitter className="text-snow-400" /> Cadastre-se com Twitter.
             </Button>
 
             <Button
@@ -115,8 +136,7 @@ export default function Register() {
               sizes="sm"
               className="flex flex-row h-10 py-0 flex-grow-0 flex-shrink-0 w-auto gap-2 text-snow-600"
             >
-              <FaApple className="text-snow-400"></FaApple> Cadastre-se com
-              Apple.
+              <FaApple className="text-snow-400" /> Cadastre-se com Apple.
             </Button>
           </div>
         </div>
