b707dc70d3c69b710f20db7b9f4b9b4c2644784b
retr0lbb <retr0lbb@gmail.com>
2024-10-08 18:10:37 -0300

    feat: add header to profile

--- src/app/user/[user]/profile/page.tsx
+++ src/app/user/[user]/profile/page.tsx
@@ -1,22 +1,34 @@
 import Image from 'next/image'
 import PROFILEPIC from '@/static/ralsei.jpg'
+import BGIMAGE from '@/static/bg-image.webp'
+import TabSwitch from '@/components/tabs-switch/index'
 
 export default function ProfilePage() {
   return (
     <section className="w-full h-full flex flex-col gap-2 p-5">
-      <div className="flex bg-red-500 w-full items-center justify-around">
-        <div className="flex items-center justify-center">
+      <div className="flex w-full items-center justify-around relative">
+        <Image
+          src={BGIMAGE}
+          alt="your wallpaper"
+          className="absolute inset-0 w-full h-full object-cover "
+        />
+        <div className="flex w-full items-center justify-center gap-4 z-10 bg-snow-900/60 p-2">
           <Image
             alt="your profile picture"
             className="rounded-xl size-48"
             src={PROFILEPIC}
           />
-          <div>
-            <h1>retr0lbb</h1>
-            <p>Henrique Barbosa Sampaio</p>
+          <div className="flex flex-col text-snow-400 w-full">
+            <h1 className="text-7xl font-bold leading-none">retr0lbb</h1>
+            <p className="text-sm text-snow-600">Henrique Barbosa Sampaio</p>
+            <p className="text-md text-snow-600">
+              lvl <span className="text-ofice_green font-bold text-md">48</span>
+            </p>
           </div>
         </div>
       </div>
+
+      <TabSwitch.root />
     </section>
   )
 }
---
+++ src/components/tabs-switch/index.tsx
@@ -0,0 +1,7 @@
+function root() {
+  return <div className="w-full h-full bg-blue-400">a</div>
+}
+
+export default {
+  root,
+}
